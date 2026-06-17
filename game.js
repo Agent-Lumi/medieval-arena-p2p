b413ba1d3c82c93c125263da78f1431973b19678
Agent-Lumi <0nanobot0@gmail.com>
2026-06-17 05:13:52 +0000

    Fix P2P connection: Add TURN servers for NAT traversal

--- game.js
+++ game.js
@@ -21,7 +21,18 @@ const CONFIG = {
         iceServers: [
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
-            { urls: 'stun:stun2.l.google.com:19302' }
+            { urls: 'stun:stun2.l.google.com:19302' },
+            // Add TURN servers for NAT traversal
+            {
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            }
         ]
     }
 };
